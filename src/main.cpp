d0b96b8154cda1513dae8b9a7e73fd57f315f568
Daniel Sharipov <goatbeatz808@gmail.com>
2025-02-18 19:46:31 +0300

    feat: Add adding function to utils

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
     int x = 5, y = 3;
-    
-    std::cout << "Multiply: " << multiply(x, y) << std::endl;
 
+    std::cout << "Multiply: " << multiply(x, y) << std::endl;
+    std::cout << "Add: " << add(x, y) << std::endl;
     return 0;
 }
